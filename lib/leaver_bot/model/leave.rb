83a7d605a9cca63a561042448129903e542c1710
Rizky Harlistyo <rizky.harlistyo@gmail.com>
2018-05-06 18:35:10 +0700

    change leave redis

--- lib/leaver_bot/model/leave.rb
+++ lib/leaver_bot/model/leave.rb
@@ -8,40 +8,32 @@ module LeaverBot
         expireat = day.next_day(2).to_time.to_i # 2 days from leave day
         key      = generate_key(day)
 
-        $redis.sadd(key, username)
+        $redis.hset(key, username, type)
         $redis.expireat(key, expireat)
-        $redis.hset(LEAVE_TYPE_KEY, username, type)
       end
     end
 
-    def self.check_status(username)
-      key = generate_key(Date.today)
+    def self.check_status(username, date = Date.today)
+      key = generate_key(date)
 
-      return nil unless $redis.sismember(key, username)
+      return nil unless $redis.hget(key, username)
 
-      if $redis.hget(LEAVE_TYPE_KEY, username) == 'cuti'
-        "#{username} - cuti"
-      elsif $redis.hget(LEAVE_TYPE_KEY, username) == 'sakit'
-        "#{username} - sakit"
-      elsif $redis.hget(LEAVE_TYPE_KEY, username) == 'remote'
-        "@#{username} - remote"
-      else
-        nil
-      end
+      leave_type = $redis.hget(key, username)
+
+      "#{leave_type == 'remote' ? "@" : ""}#{username} - #{leave_type}"
     end
 
     def self.check_leave(username)
       key = generate_key(Date.today)
 
-      $redis.sismember(key, username)
+      $redis.hget(key, username)
     end
 
     def self.remove(username, duration)
       duration.times do |d|
         key = generate_key(Date.today.next_day(d))
 
-        $redis.srem(key, username)
-        $redis.hdel(LEAVE_TYPE_KEY, username)
+        $redis.hdel(key, username)
       end
     end
 
@@ -54,7 +46,7 @@ module LeaverBot
     end
 
     def self.cache(key)
-      $redis.smembers(key).join(", ")
+      $redis.hkeys(key).join(", ")
     end
 
     def self.cache_type(username)
